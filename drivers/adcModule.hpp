3d738719402a62b948edb56dbbdd8eb9f1aaaa9f
Samuel Martel <martelsamuel00@gmail.com>
2021-11-22 10:17:29 -0500

    Documentation for AdcModule
    Add: Documentation for AdcModule, member description, method utilization

--- drivers/adcModule.hpp
+++ drivers/adcModule.hpp
@@ -25,6 +25,9 @@
 #        include <functional>
 #        include <map>
 
+/**
+ * @brief Controls a hardware ADC on the STM32.
+ */
 class AdcModule : public cep::Module
 {
 public:
@@ -35,16 +38,53 @@ public:
     void                             Run() override;
     [[nodiscard]] const std::string& GetLabel() const override { return m_label; }
 
+    /**
+     * @brief Adds a callback that willed be invoked whenever the conversion of all channel is
+     * completed.
+     * @param name The name to assign to the callback. This name is used to keep track of the
+     * callback.
+     * @param cb The callback function. It must take an @c AdcModule* as input, and return nothing.
+     */
     [[maybe_unused]] void AddConvCpltCallback(const std::string&                     name,
                                               const std::function<void(AdcModule*)>& cb);
+
+    /**
+     * @brief Adds a callback that willed be invoked whenever an error is detected by the ADC.
+     * @param name The name to assign to the callback. This name is used to keep track of the
+     * callback.
+     * @param cb The callback function. It must take an @c AdcModule* as input, and return nothing.
+     */
     [[maybe_unused]] void AddErrorCallback(const std::string&                     name,
                                            const std::function<void(AdcModule*)>& cb);
 
+    /**
+     * @brief Removes a callback called @c name from the module.
+     * @param name The name of the callback to remove.
+     *
+     * @attention Do not remove a callback from within a callback!
+     */
     [[maybe_unused]] void RemoveCallback(const std::string& name);
 
+    /**
+     * @brief Starts the continuous conversion on all channels.
+     *
+     * This is only stopped by calling @b AdcModule::Stop
+     */
     void Start();
+
+    /**
+     * @brief Stops the ADC, terminating all updates of channels.
+     */
     void Stop();
 
+    /**
+     * @brief Gets the tension, in volt, measured by the ADC at the desired channel.
+     * @param channel The channel to read.
+     * @return The value read on that channel.
+     *
+     * @attention The range of @b channel must be within 0 and the number of channels of the ADC.
+     * Specifying a @b channel over the number of channels will result undefined behavior.
+     */
     [[nodiscard]] float GetChannelReading(size_t channel) const;
 
 private:
