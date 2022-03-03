2af8ddcbd9b0edd3bec22104aec793de0053e29c
Samuel Martel <martelsamuel00@gmail.com>
2022-03-03 18:06:23 -0500

    Started implementing Events
    Add: Generic Event
    Add: External Events
    Add: Data Event
    Add: Event dispatcher

---
+++ defines/Events/ExtEvent.h
@@ -0,0 +1,41 @@
+/**
+ * @file    ExtEvent.h
+ * @author  Samuel Martel
+ * @date    2022-03-03
+ * @brief
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <a href=https://www.gnu.org/licenses/>https://www.gnu.org/licenses/<a/>.
+ */
+#ifndef NILAI_EVENTS_EXTEVENT_H
+#define NILAI_EVENTS_EXTEVENT_H
+
+#if defined(NILAI_USE_EVENTS)
+
+#    include "GenericEvent.h"
+
+namespace cep::Events
+{
+/**
+ * @brief External event triggered by a GPIO or by software.
+ */
+struct ExtEvent : public Event
+{
+    //! Was the event triggered by software?
+    bool IsFromSoft = false;
+
+    //! Which channel is the event on?
+    uint8_t SrcCh = 0;
+};
+}    // namespace cep::Events
+
+#endif
+
+#endif    // NILAI_EVENTS_EXTEVENT_H
--- processes/application.cpp
+++ processes/application.cpp
@@ -10,7 +10,93 @@
  */
 #include "application.hpp"
 
-[[noreturn]] void cep::AbortionHandler(int signal)
+#if defined(NILAI_USE_EVENTS)
+#    include "../defines/macros.hpp"
+#endif
+
+namespace cep
+{
+#if defined(NILAI_USE_EVENTS)
+static Application* s_app = nullptr;
+#endif
+
+Application::Application()
+{
+    std::signal(SIGABRT, &AbortionHandler);
+
+#if defined(NILAI_USE_EVENTS)
+    s_app = this;
+    for (auto& event : m_callbacks)
+    {
+        for (auto& cb : event)
+        {
+            cb = &DefaultEventCallback;
+        }
+    }
+#endif
+}
+
+#if defined(NILAI_USE_EVENTS)
+size_t Application::RegisterEventCallback(Events::EventTypes               event,
+                                          const Application::CallbackFunc& cb)
+{
+    CEP_ASSERT(cb, "Callback is not valid!");
+
+    return InsertCallback(m_callbacks[(size_t)event], cb);
+}
+
+void Application::UnregisterEventCallback(Events::EventTypes event, size_t id)
+{
+    CEP_ASSERT(id < s_maxEventCbCount, "Invalid callback ID!");
+
+    auto& events = m_callbacks[(size_t)event];
+
+    events[id] = DefaultEventCallback;
+}
+
+void Application::TriggerSoftEvent(Events::SoftwareEvents ch)
+{
+    Events::ExtEvent e;
+    e.IsFromSoft = true;
+    e.SrcCh = (uint8_t)ch;
+
+    DispatchEvent((Events::EventTypes)ch, &e);
+}
+
+size_t Application::InsertCallback(Application::EventCallbacks&     events,
+                                   const Application::CallbackFunc& cb)
+{
+    // Find the first free slot in the array.
+    auto it = std::find(events.begin(), events.end(), &DefaultEventCallback);
+
+    // If a slot is found:
+    if (it != events.end())
+    {
+        // Place the callback there.
+        *it = cb;
+        // Return the ID of the callback.
+        return std::distance(events.begin(), it);
+    }
+
+    // No slot found.
+    return -1;
+}
+
+void Application::DispatchEvent(Events::EventTypes e, Events::Event* data)
+{
+    const auto& events = m_callbacks[(size_t)e];
+    for (const auto& cb : events)
+    {
+        if (cb(data))
+        {
+            return;
+        }
+    }
+}
+
+#endif
+
+[[noreturn]] void AbortionHandler(int signal)
 {
     (void)signal;
     // This function will in most cases be called by the STL, in place of the standard _exit().
@@ -19,3 +105,4 @@
     {
     }
 }
+}    // namespace cep
